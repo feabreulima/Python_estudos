1ea7861ea9896c39d8f34e2a3bfe7f3e52c2632f
feabreulima <felipeabreu3731@gmail.com>
2023-01-12 16:29:10 -0300

    recursos extras em ex012

--- ex012.py
+++ ex012.py
@@ -1,8 +1,24 @@
-#Preço de um produto qualquer com 5% de desconto
+#Preço de um produto qualquer com de descontos e com juros
+
+
+print('==============================================================')
 
 product = float(input('Digite o preço do produto: R$ '))
 
 price1 = product * 0.95
 
-print(f'O novo preço do produto com 5% de desconto é: R$ {price1:.2f}')
+price2 = product * 0.90
+
+price3 = price1 * 0.90
+
+price4 = product * 1.08
+
+print(f'\nO novo preço do produto com 5% de desconto é: R$ {price1:.2f}\n')
+print(f'O preço do produto caso seje pago a vista (10% de desconto): R$ {price2} \nPreço com desconto e avista (15% de desconto): R$ {price3}\n')
+
+parcela = int(input('Digite quantas parcelas você quer pagar: '))
+price5 = (price4/parcela)
+print(f'O preço total do produto caso seja parcelado (+8%): R$ {price4} \nPreço por cada parcela: R$ {price5}')
+
+print('==============================================================')
 
